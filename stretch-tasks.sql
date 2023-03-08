5456e7309fb29224d8f5fbd1c1b307e459edf173
braym3 <126671044+braym3@users.noreply.github.com>
2023-03-08 16:32:13 +0000

    Finished task 3

--- stretch-tasks.sql
+++ stretch-tasks.sql
@@ -13,3 +13,15 @@ JOIN `ratings` ON `movies`.`id` = `ratings`.`movie_id`
 WHERE `rating` = (
 		SELECT MIN(`rating`) FROM `ratings`
         );
+
+
+-- List the unique records for Sci-Fi movies where male 24-year-old students have given 5-star ratings
+SELECT `movies`.`title` FROM `movies`
+JOIN `ratings` ON `movies`.`id` = `ratings`.`movie_id`
+JOIN `users` ON `ratings`.`user_id` = `users`.`id`
+JOIN `occupations` ON `users`.`occupation_id` = `occupations`.`id`
+JOIN `genres_movies` ON `movies`.`id` = `genres_movies`.`movie_id`
+JOIN `genres` ON `genres_movies`.`genre_id` = `genres`.`id`
+WHERE `genres`.`name` LIKE '%Sci-Fi%' AND `ratings`.`rating` = 5 AND `users`.`age` = 24 AND `occupations`.`name` LIKE '%student%'
+GROUP BY `movies`.`id`
+HAVING COUNT(`movies`.`id`) = 1;
