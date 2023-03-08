f98b2642c32b52983e98f7b632c9b5e54d0abd71
braym3 <126671044+braym3@users.noreply.github.com>
2023-03-08 15:13:21 +0000

    Finished task 2

--- join-tasks.sql
+++ join-tasks.sql
@@ -6,4 +6,8 @@ JOIN `film_category` ON `film`.`film_id` = `film_category`.`film_id`
 JOIN `category` ON `film_category`.`category_id` = `category`.`category_id`
 GROUP BY `category`.`category_id`;
 
+
 -- Which last names appear more than once?
+SELECT `last_name`, COUNT(`last_name`) AS `occurences` FROM `actor`
+GROUP BY `last_name`
+HAVING `occurences` > 1;
