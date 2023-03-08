545665471367af9fe508e020b2234df90391a735
braym3 <126671044+braym3@users.noreply.github.com>
2023-03-08 15:08:57 +0000

    Finished first task

---
+++ join-tasks.sql
@@ -0,0 +1,9 @@
+USE `sakila`;
+
+-- What is the average running time of films by category?
+SELECT `category`.`name`, AVG(`film`.`length`) AS `average_length`  FROM `film`
+JOIN `film_category` ON `film`.`film_id` = `film_category`.`film_id`
+JOIN `category` ON `film_category`.`category_id` = `category`.`category_id`
+GROUP BY `category`.`category_id`;
+
+-- Which last names appear more than once?
