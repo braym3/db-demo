225a3cc855ff40b9f82d86308b189e21b5b7e250
braym3 <126671044+braym3@users.noreply.github.com>
2023-03-08 11:37:56 +0000

    Added re-done tasks using nested queries

--- aggregate-demo.sql
+++ aggregate-demo.sql
@@ -52,5 +52,26 @@ WHERE `release_year` = '2010';
 
 
 -- Which last names are not repeated?
+-- DISTINCT only pulls first unique one it finds - not for this situation
 
 SELECT DISTINCT `last_name` FROM `actor`;
+
+
+
+-- Re-doing tasks with nested queries
+
+-- Which actor has appeared in the most films?
+SELECT CONCAT(`first_name`, ' ', `last_name`) AS `full_name` FROM `actor`
+WHERE `actor_id` = (
+	SELECT `actor_id` FROM `film_actor` 
+    GROUP BY `actor_id`
+    ORDER BY COUNT(`actor_id`) DESC LIMIT 1
+);
+
+
+ -- Find the movie(s) with the longest runtime
+ SELECT `title`, `length` FROM `film`
+ WHERE `length` = (
+	SELECT `length` FROM `film`
+    ORDER BY `length` DESC LIMIT 1
+ );
